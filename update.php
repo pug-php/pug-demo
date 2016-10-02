c2a51954962d02d252cabff891bfd085b0b12fa5
Kyle <kylekatarnls@gmail.com>
2016-10-02 10:29:18 +0000

    Update with composer

--- update.php
+++ update.php
@@ -1,17 +1,25 @@
 <?php
 
-$lastUpdate = filemtime(__DIR__ . '/vendor/autoload.php');
-
-if (time() - $lastUpdate >= 6 * 3600) {
-    shell_exec('composer update');
+function dumpVersion() {
+    if (!file_exists(__DIR__ . '/var/cache')) {
+        mkdir(__DIR__ . '/var/cache', 0777, true);
+    }
 
     $info = shell_exec('composer show pug-php/pug');
     $info = explode('versions : * ', $info, 2);
     $info = explode("\n", $info[1], 2);
     $version = $info[0];
 
-    chmod(__DIR__ . '/var', 0777);
-    chmod(__DIR__ . '/var/cache', 0777);
     chmod(__DIR__ . '/var/cache/pug-version.txt', 0666);
     file_put_contents(__DIR__ . '/var/cache/pug-version.txt', $version);
+}
+
+$lastUpdate = filemtime(__DIR__ . '/vendor/autoload.php');
+
+if (in_array('--dump-version', $argv)) {
+    dumpVersion();
+    
+} elseif (time() - $lastUpdate >= 6 * 3600) {
+    shell_exec('composer update');
+    dumpVersion();
 }
