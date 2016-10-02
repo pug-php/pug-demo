4c844cb7112015e00e1d01b6a8052ccfd4dba457
Kyle <kylekatarnls@gmail.com>
2016-10-02 10:19:53 +0000

    Update with composer

--- update.php
+++ update.php
@@ -10,5 +10,8 @@ if (time() - $lastUpdate >= 6 * 3600) {
     $info = explode("\n", $info[1], 2);
     $version = $info[0];
 
+    chmod(__DIR__ . '/var', 0777);
+    chmod(__DIR__ . '/var/cache', 0777);
+    chmod(__DIR__ . '/var/cache/pug-version.txt', 0666);
     file_put_contents(__DIR__ . '/var/cache/pug-version.txt', $version);
 }
