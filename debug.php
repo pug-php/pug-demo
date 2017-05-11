4fede0797c7316651b67c10b8adf09181e83c28f
Kyle <kylekatarnls@gmail.com>
2017-05-11 16:35:03 +0000

    Add debug page

---
+++ debug.php
@@ -0,0 +1,19 @@
+<?php
+
+function dumpDirectory($dir, $indent = 0) {
+    echo str_repeat(' ', $indent * 3) . basename($dir) . "\n";
+    if (is_dir($dir) && $indent < 5) {
+        foreach (scandir($dir) as $element) {
+            if ($element !== '.' && $element !== '..') {
+                dumpDirectory($dir . '/' . $element, $indent + 1);
+            }
+        }
+    }
+}
+
+echo shell_exec('node --version');
+
+echo '<pre>';
+dumpDirectory(__DIR__ . '/vendor/nodejs-php-fallback');
+dumpDirectory(__DIR__ . '/vendor/pug-php');
+echo '<pre>';
